96749eb16ab628da8becebe76fbfd455b0edb7d9
SergheiCucerovschii <74251573+SergheiCucerovschii@users.noreply.github.com>
2021-06-06 17:00:55 +0300

    Update jsonstorage.py

--- ProxyPattern/jsonstorage.py
+++ ProxyPattern/jsonstorage.py
@@ -8,7 +8,7 @@ class JSONStorage:
     def save(self, data):
         with open("data.json", "w") as json_file:
             json.dump(data, json_file)
-
+        json_file.close()
 
     def load(self):
         with open('data.json', "r") as json_file:
